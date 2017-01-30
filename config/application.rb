a44aa3f5f011a79f27080a12448bcbe82fb80def
Ian Bibby <ian.bibby@gmail.com>
2017-01-30 00:27:03 -0500

    Add email constraint and use the sql schema format

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module Shine
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use SQL for the schema
+    config.active_record.schema_format = :sql
   end
 end
